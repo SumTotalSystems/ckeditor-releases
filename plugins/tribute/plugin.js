32d35f2416d112f91890aaab05dbce5a0be3a1d4
Timothy O'Brien <Timothy.OBrien@sumtotalsystems.com>
2020-03-04 10:19:51 -0500

    SS-4067: Added confighelper plugin for ckeditor to support placeholder text, created tribtue plugin to support tribute component on the ckeditor iframe

---
+++ plugins/tribute/plugin.js
@@ -0,0 +1,37 @@
+﻿CKEDITOR.plugins.add('tribute', {
+	icons: 'timestamp',
+	init: function (editor) {
+		var pluginDirectory = this.path;
+		editor.addContentsCss(pluginDirectory + 'styles/tribute.css');
+
+		editor.on('contentDom', function () {
+
+			var tributeOptions = editor.config.tributeOptions || {
+				allowSpaces: true,
+				trigger: '%',
+				selectTemplate: function (item) {
+					return '%' + item.original.value + '%';
+				}
+			};
+			tributeOptions["iframe"] = editor.document.getWindow().$;
+			tributeOptions["values"] = editor.config.tributeValues || [];
+
+			var tributeInstance = new Tribute(tributeOptions);
+			tributeInstance.attach(editor.document.getBody().$);
+
+			editor.editable().on('keydown', function (e) {
+				if (e.data.$.which == 13 && tributeInstance.isActive) {
+					return false;
+				}
+			}, null, null, 5);
+
+			editor.on('blur', function (evt) {
+				tributeInstance.hideMenu();
+			});
+
+			editor.on('maximize', function (evt) {
+				tributeInstance.hideMenu();
+			});
+		});
+	}
+});
